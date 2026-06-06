d229d769d066703c2524ecedae64cf46d9a2a86d
josephslaughter808 <josephslaughter13@gmail.com>
2026-06-06 15:34:34 -0500

    push

---
+++ week06/Shapes/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 3, 5));
+        shapes.Add(new Circle("Green", 2.5));
+        shapes.Add(new Square("Yellow", 6));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} - {shape.GetArea()}");
+        }
+    }
+}
